b7774283d4f9abdb91706ec8b164805a7e039cfe
Valentin Dusollier <valentin.dusollier@icloud.com>
2020-09-01 08:33:43 +0200

    Clean main.swift

--- Sources/BetterHyperplanning/main.swift
+++ Sources/BetterHyperplanning/main.swift
@@ -5,7 +5,4 @@ fileprivate let logger = Logger(label: "root")
 
 logger.info("Initializing the server...")
 
-print("dzdz".isSubjectCode())
-print("S-MATH-987".isSubjectCode())
-
 try HTTPServer.launch(name: "httpserver", port: 8000, routes: [calendarRoute, uiRoute])
